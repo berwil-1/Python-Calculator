b9fb42bb7a95fcc25d44597eb0d4d6fce4aec8d0
CoolJWB <berghwilliam@gmail.com>
2022-11-03 19:36:31 +0100

    Remove spaces in input

---
+++ calculator.py
@@ -0,0 +1,116 @@
+#
+# Copyright 2022 CoolJWB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import math
+import numpy
+import re as regex
+
+sanetize = lambda a : float(a)
+
+functions = {
+    # Programming functions
+    'abs': lambda a : abs(sanetize(a)),
+    'floor': lambda a : math.floor(sanetize(a)),
+    'ceil': lambda a : math.ceil(sanetize(a)),
+    'round': lambda a : math.floor(sanetize(a) + 0.5),
+    
+    # Logarithmic functions
+    'log': lambda a : math.log10(sanetize(a)),
+    'ln': lambda a : math.log(sanetize(a)),
+    'exp': lambda a : math.exp(sanetize(a)),
+
+    # Trigonometric functions
+    'rad': lambda a : math.radians(sanetize(a)),
+    'deg': lambda a : math.degrees(sanetize(a)),
+    'sin': lambda a : math.sin(sanetize(a)),
+    'asin': lambda a : math.asin(sanetize(a)),
+    'cos': lambda a : math.cos(sanetize(a)),
+    'acos': lambda a : math.acos(sanetize(a)),
+    'tan': lambda a : math.tan(sanetize(a)),
+    'atan': lambda a : math.atan(sanetize(a))
+}
+
+symbols = {
+    '%': lambda a, b : sanetize(a) % sanetize(b),
+    '\\': lambda a, b : sanetize(a) ** (1.0 / sanetize(b)),
+    '^': lambda a, b : sanetize(a) ** sanetize(b),
+    '/': lambda a, b : sanetize(a) / sanetize(b),
+    '*': lambda a, b : sanetize(a) * sanetize(b),
+    '-': lambda a, b : sanetize(a) - sanetize(b),
+    '+': lambda a, b : sanetize(a) + sanetize(b),
+    '=': lambda a, b : 1 if sanetize(a) == sanetize(b) else 0,
+    '!': lambda a, b : 0 if sanetize(a) == sanetize(b) else 1,
+    '<': lambda a, b : 1 if sanetize(a) < sanetize(b) else 0,
+    '>': lambda a, b : 1 if sanetize(a) > sanetize(b) else 0
+}
+
+def format(text):
+    return regex.findall(
+        # Locate a number or
+        "([-]?[0-9]*[.]?[0-9]+|" +
+
+        # a symbol or,
+        "[><!=+\-*/^\\\\%]|" +
+
+        # a function.
+        "[a-z]+)"
+    , text)
+
+def calc(text):
+    split = format(text)
+    array = numpy.array(split)
+
+    # Iterate over functions available.
+    for function in functions:
+        count = split.count(function)
+        for _ in range(count):
+            # Find all function indices.
+            indices = numpy.where(array == function)[0]
+
+            for index in indices:
+                    result = functions[function](split[index + 1])
+                    text = text.replace(function + " " + split[index + 1], str(result)).replace(function + split[index + 1], str(result))
+            split = format(text)
+            array = numpy.array(split)
+
+    # Iterate over symbols available.
+    for symbol in symbols:
+        count = split.count(symbol)
+        for _ in range(count):
+            # Find all symbol indices.
+            indices = numpy.where(array == symbol)[0]
+
+            for index in indices:
+                result = symbols[symbol](split[index - 1], split[index + 1])
+                text = text.replace(split[index - 1] + " " + symbol + " " + split[index + 1], str(result)).replace(split[index - 1] + symbol + split[index + 1], str(result))
+            split = format(text)
+            array = numpy.array(split)
+
+    return text
+
+def main():
+    while True:
+        text = "".join(input(">> ").split())
+        if text.upper() == "EXIT": break
+
+        for _ in range(text.count("(")):
+            start = text.rfind("(") + 1
+            result = text[start:text.find(")", start)]
+            text = text.replace("(" + result + ")", ("*" if text[start - 2].isdigit() else "") + str(calc(result)))
+        print(calc(text))
+
+if __name__ == "__main__":
+    main()
